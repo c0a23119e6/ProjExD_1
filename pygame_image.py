beeef923763b2ad30ed0dfb77778939260ef3053
kohykw <c0a23119e6@edu.teu.ac.jp>
2024-09-24 16:24:39 +0900

    練習8:こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,19 +13,30 @@ def main():
     bg2_img = pg.transform.flip(bg_img,True,False) #7-1
     kk_img = pg.image.load("fig/3.png") #練習２
     kk_img = pg.transform.flip(kk_img,True,False) #練習２
+    kk_rct = kk_img.get_rect() #8
+    kk_rct.center = 300, 200 #8
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed() #8-3
+        if key_lst[pg.K_UP]: #上矢印キーがTrueなら
+            kk_rct.move_ip((0,-1)) #こうかとんの縦座標を-1にする
+        if key_lst[pg.K_DOWN]: #下矢印キーがTrueなら
+            kk_rct.move_ip((0,1)) #こうかとんの縦座標を1にする
+        if key_lst[pg.K_LEFT]: #左矢印キーがTrueなら
+            kk_rct.move_ip((-1,0)) #こうかとんの横座標を-1にする
+        if key_lst[pg.K_RIGHT]: #右矢印キーがTrueなら
+            kk_rct.move_ip((1,0)) #こうかとんの縦座標を1にする
         x = -(tmr%3200) #6
         screen.blit(bg_img, [x, 0]) #6
         screen.blit(bg2_img, [x+1600, 0]) #7-2
         screen.blit(bg_img, [x+3200, 0]) #7-2
         screen.blit(bg2_img, [x+4800, 0]) #7-2
-        screen.blit(kk_img, [300, 200]) #4
+        screen.blit(kk_img, kk_rct) #8-5
         pg.display.update()
         tmr += 1        
-        clock.tick(200) #5
+        clock.tick(400) #5
 
 
 if __name__ == "__main__":
