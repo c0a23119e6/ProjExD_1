05066223801b85dc8dce21b69a37401bc616be2e
kohykw <c0a23119e6@edu.teu.ac.jp>
2024-09-24 16:04:56 +0900

    練習７：ワープ解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,18 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg2_img = pg.transform.flip(bg_img,True,False) #7-1
     kk_img = pg.image.load("fig/3.png") #練習２
     kk_img = pg.transform.flip(kk_img,True,False) #練習２
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%800) #6
+        x = -(tmr%3200) #6
         screen.blit(bg_img, [x, 0]) #6
+        screen.blit(bg2_img, [x+1600, 0]) #7-2
+        screen.blit(bg_img, [x+3200, 0]) #7-2
+        screen.blit(bg2_img, [x+4800, 0]) #7-2
         screen.blit(kk_img, [300, 200]) #4
         pg.display.update()
         tmr += 1        
